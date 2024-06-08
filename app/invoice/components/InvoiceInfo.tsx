72a0adf33f697baca7cd73d3090d4b69f9fbbc87
Daniel Y. <daniel072.dy@gmail.com>
2024-06-08 19:48:03 +0330

    handle InvoiceInfo submittion

--- app/invoice/components/InvoiceInfo.tsx
+++ app/invoice/components/InvoiceInfo.tsx
@@ -1,29 +1,40 @@
+"use client";
+
 import { Box, Button, Flex, Text, TextField } from "@radix-ui/themes";
+import { useForm } from "react-hook-form";
 
 const invoiceInfo = () => {
+  const { register, handleSubmit } = useForm();
+
   return (
-    <form>
+    <form onSubmit={handleSubmit((data) => console.log(data))}>
       <Flex direction="column" gap="5">
-        <Flex gap="5" align="end">
+        <Flex gap="5">
           <Box>
             <Text as="label">Invoice Number</Text>
-            <TextField.Root size="3" />
+            <TextField.Root
+              type="number"
+              size="3"
+              {...register("invoiceNumber", { valueAsNumber: true })}
+            />
           </Box>
 
           <Box flexGrow="1">
-            <Text as="label">Compant Name</Text>
-            <TextField.Root size="3" />
+            <Text as="label">Company Name</Text>
+            <TextField.Root size="3" {...register("companyName")} />
+          </Box>
+        </Flex>
+
+        <Flex gap="5" align="end">
+          <Box flexGrow="1">
+            <Text as="label">Invoice Description</Text>
+            <TextField.Root size="3" {...register("invoiceDescription")} />
           </Box>
 
           <Button size="3" type="submit" color="blue">
             Save Invoice
           </Button>
         </Flex>
-
-        <Box>
-          <Text as="label">Invoice Description</Text>
-          <TextField.Root size="3" />
-        </Box>
       </Flex>
     </form>
   );
