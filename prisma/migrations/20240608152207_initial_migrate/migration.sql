4cd939eeb9611726215dfb3a74a9f60a0e967b31
Daniel Y. <daniel072.dy@gmail.com>
2024-06-08 18:53:15 +0330

    Setup prisma

---
+++ prisma/migrations/20240608152207_initial_migrate/migration.sql
@@ -0,0 +1,34 @@
+-- CreateTable
+CREATE TABLE `Product` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Invoice` (
+    `id` VARCHAR(191) NOT NULL,
+    `invoiceNumber` INTEGER NOT NULL,
+    `invoiceDescription` TEXT NOT NULL,
+    `companyName` VARCHAR(191) NOT NULL,
+    `totalPrice` INTEGER NOT NULL,
+
+    UNIQUE INDEX `Invoice_invoiceNumber_key`(`invoiceNumber`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `_InvoiceToProduct` (
+    `A` VARCHAR(191) NOT NULL,
+    `B` INTEGER NOT NULL,
+
+    UNIQUE INDEX `_InvoiceToProduct_AB_unique`(`A`, `B`),
+    INDEX `_InvoiceToProduct_B_index`(`B`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `_InvoiceToProduct` ADD CONSTRAINT `_InvoiceToProduct_A_fkey` FOREIGN KEY (`A`) REFERENCES `Invoice`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `_InvoiceToProduct` ADD CONSTRAINT `_InvoiceToProduct_B_fkey` FOREIGN KEY (`B`) REFERENCES `Product`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
