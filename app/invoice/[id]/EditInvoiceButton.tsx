21dcf47e450467a45c9cb6cc564d51a70c5e6db9
Daniel Y. <daniel072.dy@gmail.com>
2024-06-11 22:30:11 +0330

    Build EditInvoiceButton component

---
+++ app/invoice/[id]/EditInvoiceButton.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { Button } from "@radix-ui/themes";
+import { useRouter } from "next/navigation";
+import React from "react";
+
+interface Props {
+  invoiceId: string;
+}
+
+const EditInvoiceButton = ({ invoiceId }: Props) => {
+  const router = useRouter();
+  return (
+    <Button
+      className="!transition-all !cursor-pointer"
+      onClick={() => router.push(`invoice/edit/${invoiceId}`)}
+    >
+      Edit Invoice
+    </Button>
+  );
+};
+
+export default EditInvoiceButton;
